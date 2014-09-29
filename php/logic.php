0ddfe309031e16163fdcfeab1a5b88c1c48b2ab2
Wil Birkmaier <wil@birkmaier.org>
2014-09-28 23:55:44 -0400

    add code to generate random word list combination - first draft

--- php/logic.php
+++ php/logic.php
@@ -11,11 +11,23 @@ if ($numWords == "") {
 	$numWords=rand(3,25);
 }
 
+$xkcdPassword="";
+
 /*Load wordlist into array and ignore newlines in file */
-$wordlist=file($filename, FILE_IGNORE_NEW_LINES);
+$wordList=file($filename, FILE_IGNORE_NEW_LINES);
+
+/*Get total number of elements in the wordlist*/
+$arrayTotal=count($wordList) - 1;
+
+
+for ($i=0; $i<=$numWords; $i++) {
+	echo $wordList[rand(0, $arrayTotal)] . " ";
+}
 
 /* Code to test array works by outputting to screen */
 /* foreach ($wordlist as $line_num => $wordlist) {
     echo "Line #<b>{$line_num}</b> : " . "<br />\n"; 
 } */
+
+
 
