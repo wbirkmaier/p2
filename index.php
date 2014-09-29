3eed68d5f27f9c2995e13e23373bd72651203530
Wil Birkmaier <wil@birkmaier.org>
2014-09-28 20:00:32 -0400

    Made basic page with PHP require, and put phpinfo function in the logic.php file to test

---
+++ index.php
@@ -0,0 +1,29 @@
+	<?php
+        error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+        ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+        ?>
+
+<!DOCTYPE html>
+
+	<!--	Dynamic Web Applications Fall 2014  			-->
+	<!--    Project Number 2		    			-->
+	<!--    Wil Birkmaier			    			-->
+	<!--    First Project with PHP creating a password Generator    -->
+
+<html>
+
+<head>
+
+	<title>Project 2 - XKCD Password Generator</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css"/>
+	<meta charset="UTF-8">
+	<meta name="robots" content="noindex">
+	<?php require "php/logic.php"; ?>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
