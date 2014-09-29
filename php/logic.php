38594e201faa38458e469909f79e341aaa39b9bf
Wil Birkmaier <wil@birkmaier.org>
2014-09-28 22:33:58 -0400

    New dictionary wordlist added and logic php updated to load wordlist into an array

--- php/logic.php
+++ php/logic.php
@@ -1,2 +1,9 @@
 <?php
-$variable = "test";
+$filename="dictionary/wordlist";
+
+$wordlist=file($filename, FILE_IGNORE_NEW_LINES);
+
+foreach ($wordlist as $line_num => $wordlist) {
+    echo "Line #<b>{$line_num}</b> : " . "<br />\n";
+}
+
