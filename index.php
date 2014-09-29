1febb74146e65a1b191add2fa705f40061020283
Wil Birkmaier <wil@birkmaier.org>
2014-09-29 01:37:10 -0400

    Removed debug code for php errors

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-	<?php
-        error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-        ini_set('display_errors', 1); # Display errors on page (instead of a log file)
-        ?>
-
 <!DOCTYPE html>
 
 	<!--	Dynamic Web Applications Fall 2014  			-->
