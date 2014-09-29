7a72f9a80cfc361aaf755617c7d6699c0046042b
Wil Birkmaier <wil@birkmaier.org>
2014-09-29 01:32:44 -0400

    Code added for special characters check, now need to optimize code

--- php/logic.php
+++ php/logic.php
@@ -3,16 +3,23 @@
 /*Variable that points to wordlist*/
 $filename="dictionary/wordlist";
 
+/*Variable that points to special character list*/
+$specialFilename="dictionary/speciallist";
+
+
 /*Max password length*/
 $maxPassword="10";
 
 /*Set Password Length*/
 $numWords=htmlspecialchars($_POST["passwordLength"]);
 
+/*Initialize number characters boolean*/
 $addNumber=htmlspecialchars($_POST["numberChar"]);
 
+/*Initialize special characters boolean*/
 $addSpecial=htmlspecialchars($_POST["specialChar"]);
 
+/*Initialze number of words for password*/
 if ($numWords == "" || $numWords > $maxPassword) {
 	$numWords=rand(3,10);
 }
@@ -23,8 +30,14 @@ $xkcdPassword="";
 /*Load wordlist into array and ignore newlines in file */
 $wordList=file($filename, FILE_IGNORE_NEW_LINES);
 
+/*Load special character list into array and ignore newlines in file */
+$specialList=file($specialFilename, FILE_IGNORE_NEW_LINES);
+
 /*Get total number of elements in the wordlist*/
 $arrayTotal=count($wordList) - 1;
+
+/*Get total number of element in the special word list*/
+$specialArrayTotal=count($specialList) - 1;
 
 /*Build password string*/
 for ($i=0; $i<$numWords; $i++) {
@@ -39,5 +52,5 @@ if ($addNumber == "true") {
 	$xkcdPassword=$xkcdPassword . rand(0,9);
 }
 if ($addSpecial == "true") {
-        $xkcdPassword=$xkcdPassword . "@";
+        $xkcdPassword=$xkcdPassword . $specialList[rand(0, $specialArrayTotal)];
 }
