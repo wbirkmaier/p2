75a8386cd89955eb3614efac1a9234e86eabe672
Wil Birkmaier <wil@birkmaier.org>
2014-09-28 23:36:00 -0400

    add logic for setting initial password length randomly

--- php/logic.php
+++ php/logic.php
@@ -1,9 +1,21 @@
 <?php
+
+/*Variable that points to wordlist*/
 $filename="dictionary/wordlist";
 
+
+/*Set Password Length*/
+$numWords=htmlspecialchars($_GET["passwordLength"]);
+
+if ($numWords == "") {
+	$numWords=rand(3,25);
+}
+
+/*Load wordlist into array and ignore newlines in file */
 $wordlist=file($filename, FILE_IGNORE_NEW_LINES);
 
-foreach ($wordlist as $line_num => $wordlist) {
-    echo "Line #<b>{$line_num}</b> : " . "<br />\n";
-}
+/* Code to test array works by outputting to screen */
+/* foreach ($wordlist as $line_num => $wordlist) {
+    echo "Line #<b>{$line_num}</b> : " . "<br />\n"; 
+} */
 
