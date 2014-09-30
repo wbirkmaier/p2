62dfa8da98c41e17292f8ce23282d68da991e116
Wil Birkmaier <wil.birkmaier@genesys.com>
2014-09-30 15:53:46 -0400

    Clean up some layout with h1 element and add more description text

--- index.php
+++ index.php
@@ -21,15 +21,14 @@
 	<h1>This page will generate a pasword based on the xkcd method of password generation.</h1>
 	<br>
 	<p>XKCD is a an online web comic that once had a strip title <a href='http://xkcd.com/936/'>Password Strength</a>.  This particular strip detailed the dificulty of coming up with and memorizing a technically hard to guess password.  The comic goes on to explain how using a word, such as "workday" and transforming it into a password such as "W0rkd@y!3" by subsituting numbers and special characters still makes a hard to remember passowrd and is statiscally easy for a computer to crack by guesing combinations.  The arguement goes on to say that by taking a varity of works and forming a non-sensical sentance would be much harder to crack, and easier to remember, do to the sheer lengthing of the number of characters.</p>
-	<br>
-	<p>This site will take this method of password generation and create one for you based on your input.</p>
+	<p>This site will take this method of password generation and create one for you based on your input. You can pick the number of words to use, to append a number and/or append a special character to the final output.</p>
 	<br>
 
 	<form action="index.php" method="post">
 
 	<fieldset>
 
-		<legend>XKCD Password Generator</legend>
+		<legend>The XKCD Password Generator</legend>
 
 		<label for="passwordLength"><b>Enter a number between 1 and 10 or click generate when blank for a random length</b></label><br>
 		<input type="text" id="passwordLength" name="passwordLength" placeholder="Enter Number"><br>
