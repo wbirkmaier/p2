90385dcfd5f3b1184de7f0216dcd49171a334ab6
Wil Birkmaier <wil.birkmaier@genesys.com>
2014-09-30 15:49:02 -0400

    Added a description for what the page does

--- index.php
+++ index.php
@@ -18,7 +18,11 @@
 </head>
 
 <body>
-	<h1>This page will generate a pasword based on the <a href='http://xkcd.com/936/'>XKCD Method</a> of password generation.</h1>
+	<h1>This page will generate a pasword based on the xkcd method of password generation.</h1>
+	<br>
+	<p>XKCD is a an online web comic that once had a strip title <a href='http://xkcd.com/936/'>Password Strength</a>.  This particular strip detailed the dificulty of coming up with and memorizing a technically hard to guess password.  The comic goes on to explain how using a word, such as "workday" and transforming it into a password such as "W0rkd@y!3" by subsituting numbers and special characters still makes a hard to remember passowrd and is statiscally easy for a computer to crack by guesing combinations.  The arguement goes on to say that by taking a varity of works and forming a non-sensical sentance would be much harder to crack, and easier to remember, do to the sheer lengthing of the number of characters.</p>
+	<br>
+	<p>This site will take this method of password generation and create one for you based on your input.</p>
 	<br>
 
 	<form action="index.php" method="post">
